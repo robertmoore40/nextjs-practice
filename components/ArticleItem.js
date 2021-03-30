eff105cee2199c1104b08661b90b7d2deb9ec48e
BigRob <robertcmoore92@gmail.com>
2021-03-30 08:57:37 -0400

    added h3 and p

--- components/ArticleItem.js
+++ components/ArticleItem.js
@@ -5,3 +5,5 @@ const ArticleItem = ({ article }) => {
     return (
         <Link href={`/article/${article.id}`}>
         <a className={articleStyles.card}>
+        <h3>{article.title} &rarr;</h3>
+        <p>{article.excerpt}</p>
