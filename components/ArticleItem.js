d8d076098929517935311179b6b865825a9d3c09
BigRob <robertcmoore92@gmail.com>
2021-03-30 08:57:22 -0400

    added article styles and id

--- components/ArticleItem.js
+++ components/ArticleItem.js
@@ -3,3 +3,5 @@ import articleStyles from '../styles/Article.module.css'
 
 const ArticleItem = ({ article }) => {
     return (
+        <Link href={`/article/${article.id}`}>
+        <a className={articleStyles.card}>
