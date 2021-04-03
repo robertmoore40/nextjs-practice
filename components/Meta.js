8c2f67a526074778b70da785494e3dd6302773db
BigRob <robertcmoore92@gmail.com>
2021-04-03 11:43:30 -0400

    added closures

--- components/Meta.js
+++ components/Meta.js
@@ -1,14 +1,14 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 const Meta = ({ title, keywords, description }) => {
-    return (
-        <Head>
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <meta name='keywords' content={keywords} />
-        <meta name='description' content={description} />
-     <meta charSet='utf-8' />
-     <link rel='icon' href='/favicon.ico' />
+  return (
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="keywords" content={keywords} />
+      <meta name="description" content={description} />
+      <meta charSet="utf-8" />
+      <link rel="icon" href="/favicon.ico" />
       <title>{title}</title>
-      </Head>
-  )
-}
+    </Head>
+  );
+};
