dafb39c98a20d9658c13a226af0090fe719ce276
BigRob <robertcmoore92@gmail.com>
2021-04-03 11:42:26 -0400

    added meta

--- components/Meta.js
+++ components/Meta.js
@@ -2,3 +2,6 @@ import Head from 'next/head'
 
 const Meta = ({ title, keywords, description }) => {
     return (
+        <Head>
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta name='keywords' content={keywords} />
