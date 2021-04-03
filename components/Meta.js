3c451db656142d1182725a850d3edd63ccece7bf
BigRob <robertcmoore92@gmail.com>
2021-04-03 11:42:59 -0400

    added link title

--- components/Meta.js
+++ components/Meta.js
@@ -7,3 +7,5 @@ const Meta = ({ title, keywords, description }) => {
         <meta name='keywords' content={keywords} />
         <meta name='description' content={description} />
      <meta charSet='utf-8' />
+     <link rel='icon' href='/favicon.ico' />
+      <title>{title}</title>
