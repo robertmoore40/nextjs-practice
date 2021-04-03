9948596bb134d4087517bb9a49a8d8eb142cc343
BigRob <robertcmoore92@gmail.com>
2021-04-03 11:42:45 -0400

    added char set

--- components/Meta.js
+++ components/Meta.js
@@ -5,3 +5,5 @@ const Meta = ({ title, keywords, description }) => {
         <Head>
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <meta name='keywords' content={keywords} />
+        <meta name='description' content={description} />
+     <meta charSet='utf-8' />
