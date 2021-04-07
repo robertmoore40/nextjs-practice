05fd4cbbde40592824154db0627d75ae565c54d5
BigRob <robertcmoore92@gmail.com>
2021-04-07 10:26:03 -0400

    added return

--- pages/index.js
+++ pages/index.js
@@ -10,12 +10,12 @@ export default function Home({ articles }) {
 }
 
 export const getStaticProps = async () => {
-    const res = await fetch(`${server}/api/articles`)
-    const articles = await res.json()
+  const res = await fetch(`${server}/api/articles`);
+  const articles = await res.json();
 
-    return {
-        props: {
-            articles,
-        },
-    }
-  }
+  return {
+    props: {
+      articles,
+    },
+  };
+};
