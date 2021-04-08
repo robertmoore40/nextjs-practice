657ce9d88960eabd6cce238f39b5f167c1f0ec9a
BigRob <robertcmoore92@gmail.com>
2021-04-08 13:15:46 -0400

    rewrote return

--- pages/_document.js
+++ pages/_document.js
@@ -2,11 +2,11 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
     render() {
-        return (
-            <Html lang='en'>
-                 <Head />
-        <body></body>
-        <Main />
-          <NextScript />
-        </body>
-      </Html>
+      return (
+        <Html lang='en'>
+          <Head />
+          <body>
+            <Main />
+            <NextScript />
+          </body>
+        </Html>
