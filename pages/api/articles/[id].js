1e81ef131549965aa545ec77264cc4e6d70eff31
BigRob <robertcmoore92@gmail.com>
2021-04-09 09:27:33 -0400

    added filter

--- pages/api/articles/[id].js
+++ pages/api/articles/[id].js
@@ -2,3 +2,7 @@ import { articles } from '../../../data'
 
 export default function handler({ query: { id } }, res) {
     const filtered = articles.filter((article) => article.id === id)
+
+
+    if (filtered.length > 0) {
+        res.status(200).json(filtered[0])
