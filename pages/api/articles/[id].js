35bfd6bc5313ae5dab3f62515f4086b34ce3c842
BigRob <robertcmoore92@gmail.com>
2021-04-09 09:28:32 -0400

    added formatting

--- pages/api/articles/[id].js
+++ pages/api/articles/[id].js
@@ -1,15 +1,13 @@
-import { articles } from '../../../data'
+import { articles } from "../../../data";
 
 export default function handler({ query: { id } }, res) {
-    const filtered = articles.filter((article) => article.id === id)
+  const filtered = articles.filter((article) => article.id === id);
 
-
-    if (filtered.length > 0) {
-        res.status(200).json(filtered[0])
-
-    } else {
-        res
-          .status(404)
-          .json({ message: `Article with the id of ${id} is not found` })
-        }
-    }
+  if (filtered.length > 0) {
+    res.status(200).json(filtered[0]);
+  } else {
+    res
+      .status(404)
+      .json({ message: `Article with the id of ${id} is not found` });
+  }
+}
